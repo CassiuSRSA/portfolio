7cb50bb3a56422af3d023972d8240eb7873664fc
Sean Brookstein <s.brookstein@gmail.com>
2022-08-18 15:33:49 +0200

    Added Submitform.co to handle form submission, removed handler for submit from JS and changed some styling

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 const cards = document.querySelectorAll(".card");
-const form = document.querySelector("form");
-const name = document.querySelector("#name");
-const email = document.querySelector("#email");
-const message = document.querySelector("#message");
 
 cards.forEach((card) =>
   card.addEventListener("click", function (e) {
@@ -13,17 +9,3 @@ cards.forEach((card) =>
     }
   })
 );
-
-form.addEventListener("submit", function (e) {
-  // e.preventDefault();
-  if (
-    name.value.length < 2 ||
-    email.value.length < 2 ||
-    message.value.length < 2 ||
-    email.value.indexOf("@") === -1
-  ) {
-    alert(`Please enter valid details`);
-  } else {
-    alert("Thank you for your submission");
-  }
-});
